85f874d5dc3c2252ed00f73fa0f78f6c270d45f6
radha <radha@param-solutions.com>
2018-04-03 17:50:46 +0530

    Initial framework code

---
+++ DevLogger/DevLogger/DevLogger.h
@@ -0,0 +1,19 @@
+//
+//  DevLogger.h
+//  DevLogger
+//
+//  Created by Orbysol on 4/3/18.
+//  Copyright © 2018 Orbysol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DevLogger.
+FOUNDATION_EXPORT double DevLoggerVersionNumber;
+
+//! Project version string for DevLogger.
+FOUNDATION_EXPORT const unsigned char DevLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DevLogger/PublicHeader.h>
+
+
---
+++ DevLogger/DevLogger/TestService.swift
@@ -0,0 +1,18 @@
+//
+//  TestService.swift
+//  DevLogger
+//
+//  Created by Orbysol on 4/3/18.
+//  Copyright © 2018 Orbysol. All rights reserved.
+//
+
+import Foundation
+
+public class TestService {
+    
+    private init()   { }
+    
+    public static func TestFunc() -> String {
+        return "Called Through pod"
+    }
+}
